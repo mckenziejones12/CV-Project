ee69b8d7f4998313bbcbeb88dc9c574275777689
McKenzie Jones <mckenzbeck12@gmail.com>
2023-10-29 12:09:19 -0600

    Change code organization and add personal info component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import { personalInfo, experience, education } from "./mockData";
-import { EditableSection } from "./EditableSection";
+import { useState } from "react";
+import { PersonalInfoForm } from "./components/PersonalInfoForm";
 
 function App() {
+  const [name, setName] = useState("McKenzie");
+  const [email, setEmail] = useState("test@gmail.com");
+  const [phoneNum, setPhoneNum] = useState("555-555-5555");
+
+  const handleNameChange = (newName) => {
+    setName(newName);
+  };
+
+  const handleEmailChange = (newEmail) => {
+    setEmail(newEmail);
+  };
+
+  const handlePhoneNumChange = (newPhoneNum) => {
+    setPhoneNum(newPhoneNum);
+  };
+
   return (
     <div id="cvContainer">
-      <div className="header">
-        <EditableSection heading="Personal Information" fields={personalInfo} />
-      </div>
-      <div>
-        <EditableSection heading="Experience" fields={experience} />
-      </div>
-      <div>
-        <EditableSection heading="Education" fields={education} />
-      </div>
+      <PersonalInfoForm
+        name={name}
+        email={email}
+        phoneNum={phoneNum}
+        onChangeName={handleNameChange}
+        onChangeEmail={handleEmailChange}
+        onChangePhoneNum={handlePhoneNumChange}
+      />
+      <p> Updated value: {name}</p>
+      <p>Updated value: {email}</p>
     </div>
   );
 }
---
+++ src/components/PersonalInfoForm.jsx
@@ -0,0 +1,40 @@
+export const PersonalInfoForm = ({
+  name,
+  email,
+  phoneNum,
+  onChangeName,
+  onChangeEmail,
+  onChangePhoneNum,
+}) => {
+  return (
+    <div className="inputPersonalInfo section">
+      <h2 className="inputSectionHeading">Personal Information</h2>
+      <label htmlFor={name}>Full Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={(e) => onChangeName(e.target.value)}
+      />
+      <br />
+      <label htmlFor={email}>Email:</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        value={email}
+        onChange={(e) => onChangeEmail(e.target.value)}
+      />
+      <br />
+      <label htmlFor={phoneNum}>Phone Number:</label>
+      <input
+        type="tel"
+        id="phoneNum"
+        name="phoneNum"
+        value={phoneNum}
+        onChange={(e) => onChangePhoneNum(e.target.value)}
+      />
+    </div>
+  );
+};
